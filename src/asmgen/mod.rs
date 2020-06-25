b872b897ebd11e2d8f9d0143a09777578100ea0d
Keonwoo Kim <keonwoo@kaist.ac.kr>
2020-06-26 02:39:21 +0900

    hw7 start

--- src/asmgen/mod.rs
+++ src/asmgen/mod.rs
@@ -9,7 +9,36 @@ impl Translate<ir::TranslationUnit> for Asmgen {
     type Target = asm::Asm;
     type Error = ();
 
-    fn translate(&mut self, _source: &ir::TranslationUnit) -> Result<Self::Target, Self::Error> {
-        todo!("homework 7")
+    fn translate(&mut self, source: &ir::TranslationUnit) -> Result<Self::Target, Self::Error> {
+        let mut functions = Vec::<asm::Section<asm::Function>>::new();
+        let mut variables = Vec::<asm::Section<asm::Variable>>::new();
+
+        for (name, decl) in source.decls.iter() {
+            match decl {
+                ir::Declaration::Variable { dtype, initializer } => {}
+                ir::Declaration::Function {
+                    signature,
+                    definition,
+                } => {
+                    let mut blocks = Vec::<asm::Block>::new();
+
+                    let func = asm::Function { blocks };
+
+                    let section = asm::Section {
+                        header: Vec::new(),
+                        body: func,
+                    };
+
+                    functions.push(section);
+                }
+            }
+        }
+
+        let unit = asm::TranslationUnit {
+            functions,
+            variables,
+        };
+
+        Ok(asm::Asm { unit })
     }
 }
